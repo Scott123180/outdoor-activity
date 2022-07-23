98c4965a1a76a6135131fe5262cc9c531b055b6b
scott123180 <me@scotthansen.io>
2022-07-23 08:22:18 -0400

    adding fetch calls. currently broken

--- src/App.js
+++ src/App.js
@@ -9,6 +9,10 @@ function App() {
         Welcome to a weather app! 🌞
       <Weather />
       </header>
+      <footer>
+        Disclaimer: Use application at your own risk. Data and information may be innacurate. Application is in development and features may be broken. 
+        Please consult your physician before excercise.
+      </footer>
     </div>
   );
 }
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -6,9 +6,21 @@ import React from "react";
 //-74.0417513
 //(40.7337633,-74.0417513)
 
+/*
+
+- build out weather data processing layer
+- gather more info from weather gov
+    - forecast
+- add support for zip codes
+- add weather forecasting (based on weather data)
+- add outdoor activity recommendation layer
+- add emoji for current weather
+
+*/
+
 class Weather extends React.Component {
 
-    govUrl = "https://api.weather.gov"; 
+    govUrl = "https://api.weather.gov";
 
     constructor() {
         super();
@@ -20,21 +32,36 @@ class Weather extends React.Component {
         };
     }
 
+    //maybe use promise chaining
+    //TODO: figure out how to properly make a group of rest calls
     fetchData = (latitude, longitude) => {
-        this.fetchAlerts(latitude,longitude); 
+        this.fetchAlerts(latitude, longitude);
+        this.fetchZones(latitude, longitude);
     }
 
     fetchAlerts = (latitude, longitude) => {
         const url = this.govUrl + "/alerts?";
 
         fetch(url + new URLSearchParams({
-            message_type : "alert",
-            point : latitude + "," + longitude
+            message_type: "alert",
+            point: latitude + "," + longitude
         }), {
             method: "GET",
         })
-        .then(response => response.json())
-        .then(json => this.setState({alerts : json.features}));
+            .then(response => response.json())
+            .then(json => this.setState({ alerts: json.features }));
+    }
+
+    fetchZones = (latitude, longitude) => {
+        const url = this.govUrl + "/zones?"
+
+        fetch(url + new URLSearchParams({
+            point: latitude + "," + longitude
+        }, {
+            method: "GET",
+        })
+            .then(response => response.json())
+            .then(json => console.log(JSON.stringify(json))));
     }
 
     componentDidMount() {
@@ -55,13 +82,11 @@ class Weather extends React.Component {
 
     render() {
 
-        console.log(this.state.alerts);
-
         return (
             <div>
                 <p>Your coordinates are ({this.state.latitude}, {this.state.longitude})</p>
                 <p>There are {this.state.alerts.length} alerts for your area.</p>
-                <p>{this.state.alerts.map(a => React.createElement('p', {}, a.properties.event))}</p>
+                {this.state.alerts.map(a => React.createElement('p', { style: { color: "red" } }, a.properties.event))}
             </div>
         );
     }
