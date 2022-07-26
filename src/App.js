903c6d07863f03f7ebbbb41cd1936383fe0c003c
scott123180 <me@scotthansen.io>
2022-07-25 21:33:17 -0400

    removing default icons and adding a current weather card

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import Weather from './components/Weather';
 
@@ -6,12 +5,13 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        Welcome to a weather app! 🌞
+        Welcome to a weather app!
       <Weather />
       </header>
       <footer>
         Disclaimer: Use application at your own risk. Data and information may be innacurate. Application is in development and features may be broken. 
-        Please consult your physician before excercise. Weather data from weather.gov
+        Please consult your physician before excercise. Weather data from weather.gov. 
+        <a href='https://github.com/Scott123180/outdoor-activity' target="_blank" rel="noreferrer">Github and attributions.</a>
       </footer>
     </div>
   );
--- src/components/WeatherConditionEmoji.js
+++ src/components/WeatherConditionEmoji.js
@@ -7,18 +7,55 @@ WeatherConditionEmoji.js
 */
 
 import React from "react";
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
 
 class WeatherConditionEmoji extends React.Component {
-    constructor(props){
+    constructor(props) {
         super(props);
     }
 
+    
+
+    /*
+CLEAR or SUNNY	No clouds
+MOSTLY SUNNY or MOSTLY CLEAR	1/8 to 2/8 clouds
+PARTLY CLOUDY or PARTLY SUNNY	3/8 to 5/8 clouds
+MOSTLY CLOUDY (sometimes CONSIDERABLE CLOUDINESS)	6/8 to 7/8 clouds
+CLOUDY	8/8 clouds
+Also used:
+INCREASING CLOUDS
+DECREASING CLOUDS
+*/
+
     render() {
 
-        return <p style={{color: "yellow"}}>Here is the current weather conditions for your area: {this.props.textDescription}</p>
+        //return <p style={{color: "yellow"}}>Here is the current weather conditions for your area: {this.props.textDescription}</p>
 
+        const emoji = "picture";
+
+        return (
+            <Card sx={{ minWidth: 275 }}>
+                <CardContent>
+                    <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                        Current weather
+                    </Typography>
+                    <Typography variant="h5" component="div">
+                    </Typography>
+                    {emoji}
+                    <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                        {this.props.textDescription}
+                    </Typography>
+                    <Typography variant="body2">
+                        additional description
+                    </Typography>
+                </CardContent>
+            </Card>
+        );
     }
 
 }
+
 
 export default WeatherConditionEmoji;
