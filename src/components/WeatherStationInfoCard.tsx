d951fb8fc309d698623e0c55183cc5103648883a
scott123180 <me@scotthansen.io>
2022-12-01 15:18:11 -0500

    code formatting

--- src/components/WeatherStationInfoCard.tsx
+++ src/components/WeatherStationInfoCard.tsx
@@ -4,30 +4,28 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
 
-const WeatherStationInfoCard = (props: any) => {
-        return (
-            <Card sx={{ minWidth: 275 }}>
-                <CardContent>
-                    <Typography variant="h4" component="div">
-                        Local Station
-                    </Typography>
-                    <Typography sx={{ mb: 1.5 }} color="text.secondary">
-                        Your coordinates: <br/>
-                        ({props.latitude}, {props.longitude}) 
-                    </Typography>
-                    <Typography sx={{ mb: 1.5 }} color="text.secondary">
-                        station: {props.station}
-                    </Typography>
-                    <Typography sx={{ mb: 1.5 }} color="text.secondary">
-                        zone: {props.zone}
-                    </Typography>
-                    <Typography sx={{ mb: 1.5 }} color="text.secondary">
-                        weather station: {props.cwa}
-                    </Typography>
-                </CardContent>
-            </Card>
-        );
-}
+const WeatherStationInfoCard = (props: any) => (
+    <Card sx={{ minWidth: 275 }}>
+        <CardContent>
+            <Typography variant="h4" component="div">
+                Local Station
+            </Typography>
+            <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                Your coordinates: <br />
+                ({props.latitude}, {props.longitude})
+            </Typography>
+            <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                station: {props.station}
+            </Typography>
+            <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                zone: {props.zone}
+            </Typography>
+            <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                weather station: {props.cwa}
+            </Typography>
+        </CardContent>
+    </Card>
+);
 
 WeatherStationInfoCard.defaultProps = {
     latitude: "",
